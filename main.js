dbf38abab5d18ea5b3e01fe8e71d561cbdc10652
Erik <uckert@outlook.com>
2020-04-11 08:06:49 +0200

    add v-show and switch between stock and out of stock images

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var app = new Vue({
     data: {
         product: 'socks',
         image: './assets/vmSocks-green-onWhite.jpg',
+        oosImage: './assets/oos-vmSocks-green-onWhite.jpg',
+        altText: 'Out of Stock',
         link: 'https://www.amazon.de/s?k=socken&__mk_de_DE=ÅMÅŽÕÑ&ref=nb_sb_noss_2',
-        inventory: 100
+        inventory: 8
     }
 })
