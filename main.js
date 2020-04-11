0a90b6fa51ce0d93dfec8ffc52e9f4776d9475cb
Erik <uckert@outlook.com>
2020-04-11 23:19:54 +0200

    finished lesson 4

--- main.js
+++ main.js
@@ -8,6 +8,17 @@ var app = new Vue({
         link: 'https://www.amazon.de/s?k=socken&__mk_de_DE=ÅMÅŽÕÑ&ref=nb_sb_noss_2',
         inventory: 8,
         onSale: true,
-        details: ['100% cotton', '0% polyester', '110% superstylisch']
+        details: ['100% cotton', '0% polyester', '110% superstylisch'],
+        variants: [
+            {
+                variantID: 1,
+                variantColor: 'green'
+            },
+            {
+                variantID: 2,
+                variantColor: 'black'
+            }
+
+        ]
     }
 })
